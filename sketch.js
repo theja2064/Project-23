4dbe3474276d995a6b0d6839904baf204257c700
theja2064 <80520832+theja2064@users.noreply.github.com>
2021-07-08 16:36:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -72,15 +72,7 @@ function draw() {
   background(0);
   
 
-	if (keyCode === LEFT_ARROW) {
-		helicopterSprite.x = helicopterSprite.x-20;
-		Matter.Body.translate(packageBody, {x:-20,y:0})
-	}
 
-	if (keyCode === RIGHT_ARROW) {
-		helicopterSprite.x = helicopterSprite.x+20;
-		Matter.Body.translate(packageBody, {x:+20,y:0})
-	}
 
 	if (keyCode === DOWN_ARROW) {
 		Matter.Body.setStatic(packageBody,false);
@@ -92,7 +84,18 @@ function draw() {
 
   
   drawSprites();
-  
-  
  
 }
+function keyPressed(){
+	if (keyCode === LEFT_ARROW) {
+		helicopterSprite.x = helicopterSprite.x-20;
+		Matter.Body.translate(packageBody, {x:-20,y:0})
+	}
+
+	if (keyCode === RIGHT_ARROW) {
+		helicopterSprite.x = helicopterSprite.x+20;
+		Matter.Body.translate(packageBody, {x:+20,y:0})
+	}
+
+}
+	
